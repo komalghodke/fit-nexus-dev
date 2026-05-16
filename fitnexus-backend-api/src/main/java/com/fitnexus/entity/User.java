04e42f7ac1cf91016c8d3186e361a05018ebc7d8
komalghodke <komalghodke99@gmail.com>
2026-05-17 00:05:00 +0530

    JWT authentication working (login tested in Postman), JwtUtil + JwtRequestFilter wired

--- fitnexus-backend-api/src/main/java/com/fitnexus/entity/User.java
+++ fitnexus-backend-api/src/main/java/com/fitnexus/entity/User.java
@@ -13,7 +13,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users") // ✅ must match DB table
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,12 +22,11 @@ public class User {
 	private String username;
 	private String email;
 	private String password;
+	private String name;
 
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	@JsonIgnoreProperties("user")
 	private List<WorkoutLog> workoutLogs;
-
-	private String name;
 
 	public Long getId() {
 		return id;
@@ -35,6 +34,30 @@ public class User {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getName() {
@@ -45,11 +68,11 @@ public class User {
 		this.name = name;
 	}
 
-	public String getEmail() {
-		return email;
+	public List<WorkoutLog> getWorkoutLogs() {
+		return workoutLogs;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setWorkoutLogs(List<WorkoutLog> workoutLogs) {
+		this.workoutLogs = workoutLogs;
 	}
 }
